cdb9f546495b77946b2431c24f8a5b56263fc85e
IvanYankov95 <user-17@Trainings-Mac-mini-4.local>
2016-04-03 09:16:44 +0300

    Created Hotel and Room DAO

--- BookingTopia/app/src/main/java/model/connectionhelper/ConnectionHelper.java
+++ BookingTopia/app/src/main/java/model/connectionhelper/ConnectionHelper.java
@@ -4,4 +4,6 @@ package model.connectionhelper;
  * Created by user-17 on 4/2/16.
  */
 public class ConnectionHelper {
+
+
 }
---
+++ BookingTopia/app/src/main/java/model/dao/RoomDAO.java
@@ -0,0 +1,7 @@
+package model.dao;
+
+/**
+ * Created by user-17 on 4/3/16.
+ */
+public class RoomDAO {
+}
