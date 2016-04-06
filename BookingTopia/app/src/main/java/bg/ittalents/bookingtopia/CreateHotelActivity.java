278394571b21ddc8731ae78d8cca5f61ebe23436
Ivo Raykov <ivo_raykow@abv.bg>
2016-04-06 18:56:42 +0300

    small changes over CreateHotel

--- BookingTopia/app/src/main/java/bg/ittalents/bookingtopia/CreateHotelActivity.java
+++ BookingTopia/app/src/main/java/bg/ittalents/bookingtopia/CreateHotelActivity.java
@@ -1,9 +1,6 @@
 package bg.ittalents.bookingtopia;
 
-import android.content.ClipData;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class CreateHotelActivity extends AbstractDrawerActivity {
 
@@ -13,7 +10,6 @@ public class CreateHotelActivity extends AbstractDrawerActivity {
         setContentView(R.layout.activity_create_hotel_drawer);
         onCreateDrawer();
         getSupportActionBar().setTitle("Create hotel");
-
 
 
     }
